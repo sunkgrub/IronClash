e5797ee363b14605e2f7f0f4c4d41be761e647b6
sunkgrub <sunkgrub@gmail.com>
2025-01-03 03:37:25 -0500

    Rescaled gun guy

--- image_conversion.py
+++ image_conversion.py
@@ -1,10 +1,10 @@
 from PIL import Image
 
 # Open your sprite image
-sprite = Image.open("Assets/Characters/Foot_guy/footGuy.png")  # Replace with your file path
+sprite = Image.open("Assets/Characters/GunGuy/gunGuy.png")  # Replace with your file path
 
 # Upscale using nearest-neighbor interpolation
 upscaled_sprite = sprite.resize((200, 300), Image.NEAREST)
 
 # Save the upscaled image
-upscaled_sprite.save("Assets/Characters/Foot_guy/sprite_upscaled.png")
+upscaled_sprite.save("Assets/Characters/GunGuy/fdasfdsafads.png")
