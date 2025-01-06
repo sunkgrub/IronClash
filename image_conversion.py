8e42b5fafb91fdf938af0b3db5ad826cff7fb688
sunkgrub <sunkgrub@gmail.com>
2025-01-06 00:51:29 -0500

    Made them bigger im gonna touch you james deeply and sincerely

--- image_conversion.py
+++ image_conversion.py
@@ -1,10 +1,10 @@
 from PIL import Image
 
 # Open your sprite image
-sprite = Image.open("Assets/Characters/GunGuy/gunGuy.png")  # Replace with your file path
+sprite = Image.open("Assets/Characters/FootGuy/footGuy2.png")  # Replace with your file path
 
 # Upscale using nearest-neighbor interpolation
 upscaled_sprite = sprite.resize((200, 300), Image.NEAREST)
 
 # Save the upscaled image
-upscaled_sprite.save("Assets/Characters/GunGuy/fdasfdsafads.png")
+upscaled_sprite.save("Assets/Characters/FootGuy/FootGuy2.png")  # Replace with your file path
